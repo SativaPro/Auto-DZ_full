d4630cecae96ed124fa09ccff5cf333812c88aed
Anastasia Prokudina <anastasia.prokudina@itqc.ru>
2025-12-29 14:55:59 +0300

    DZ-10

---
+++ lesson_10/page/login_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class LoginPage:
+    URL = "https://www.saucedemo.com/"
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def open(self):
+        self.driver.get(self.URL)
+
+    def login(self, username: str, password: str):
+        self.driver.find_element(By.ID, "user-name").send_keys(username)
+        self.driver.find_element(By.ID, "password").send_keys(password)
+        self.driver.find_element(By.ID, "login-button").click()
+
+        self.wait.until(
+            EC.presence_of_element_located((By.ID, "inventory_container"))
+        )
